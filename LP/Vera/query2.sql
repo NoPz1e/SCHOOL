23365e1e841f0fa67465a0fddb83515bf4dc6246
NoPz1e <Cristianomanuelneto2006@gmail.com>
2024-04-30 11:33:57 +0100

    Organizado

--- LP/Vera/query2.sql
+++ LP/Vera/query2.sql
@@ -4,30 +4,33 @@ CREATE DATABASE CinemaOnline;
 
 use CinemaOnline;
 
-CREATE TABLE Cinema (COD_cinema integer primary key, 
+CREATE TABLE Cinema (
+  COD_cinema integer primary key, 
   Nome_cinema VARCHAR(20), 
   Sala_cinema TINYINT, 
   Capacidade_cinema SMALLINT, 
   Cidade_cinema VARCHAR(20)
 );
 
-CREATE TABLE Filmes( COD_filme int primary key,
-     Titulo_filme VARCHAR(100),
-     Genero_filme VARCHAR(30),
-    ControloParental VARCHAR(5) default 'TODOS'
-    );
+CREATE TABLE Filmes( 
+  COD_filme int primary key,
+  Titulo_filme VARCHAR(100),
+  Genero_filme VARCHAR(30),
+  ControloParental VARCHAR(5) default 'TODOS'
+;
 
 CREATE TABLE Sessao (
-    COD_sessao INT PRIMARY KEY,
-    DATAHORA_sessao DATE,
-    PREÇO_sessa FLOAT,
-    CODFilme_sessao INT,
-    CODCinema_sessao INT,
-    FOREIGN KEY (CODFilme_sessao) REFERENCES Filmes (COD_filme),
-    FOREIGN KEY (CODCinema_sessao) REFERENCES Cinema (COD_cinema)
+  COD_sessao INT PRIMARY KEY,
+  DATAHORA_sessao DATE,
+  PREÇO_sessa FLOAT,
+  CODFilme_sessao INT,
+  FOREIGN KEY (CODFilme_sessao) REFERENCES Filmes (COD_filme),
+  CODCinema_sessao INT,
+  FOREIGN KEY (CODCinema_sessao) REFERENCES Cinema (COD_cinema)
 );
 
-CREATE TABLE Clientes (COD_cliente int primary key, 
+CREATE TABLE Clientes (
+  COD_cliente int primary key, 
   Nome VARCHAR(30), 
   Codigo_postal integer, 
   Genero ENUM('M','F','O'), 
