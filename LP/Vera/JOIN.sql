298ebe111f3dec07b09a38a29d3b7f159b59c25b
NoPz1e <Cristianomanuelneto2006@gmail.com>
2024-05-03 10:27:42 +0100

    Adicionado valores

--- LP/Vera/JOIN.sql
+++ LP/Vera/JOIN.sql
@@ -4,17 +4,27 @@ CREATE DATABASE loja;
 USE loja;
 
 CREATE TABLE IF NOT EXISTS funcionarios(
-  ID_funcionario INT PRIMARY KEY AUTO_INCREMENT,
+  ID_funcionario INT AUTO_INCREMENT,
   nome VARCHAR(30),
   morada VARCHAR(40),
-  contacto INT
+  contacto INT(9),
+  PRIMARY KEY(ID_funcionario)
 );
 
 CREATE TABLE IF NOT EXISTS clientes(
-  ID_Cliente INT PRIMARY KEY AUTO_INCREMENT,
+  ID_Cliente INT AUTO_INCREMENT,
   nome VARCHAR(30),
   morada VARCHAR(40),
-  contacto INT,
+  contacto INT(9),
   criado_por_IDFunc INT,
+  PRIMARY KEY (ID_Cliente),
   FOREIGN KEY (criado_por_IDFunc) REFERENCES funcionarios(ID_funcionario)
 );
+
+INSERT INTO funcionarios VALUES(default, 'Alberto', 'Fig da foz', 987654321),
+(default, 'Maria', 'Lisboa', 946095094),
+(default, 'Alberto', 'Coimbra', 153836737);
+
+INSERt INTO clientes VALUES(default, 'Fernando', 'faro', 635764664, 2),
+(default, 'Ana', 'viseu', 635764664, 3),
+(default, 'Rodrigo', 'Cantanhede', 635764664, 1);
